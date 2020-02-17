32fb122cf3cdc71ce7d928021acab0517a224575
carloshenche <carloshenche@metricool.com>
2020-02-17 14:44:34 +0100

    Load state machine from database

--- src/main/java/guru/springframework/msscssm/domain/Payment.java
+++ src/main/java/guru/springframework/msscssm/domain/Payment.java
@@ -2,12 +2,14 @@ package guru.springframework.msscssm.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
