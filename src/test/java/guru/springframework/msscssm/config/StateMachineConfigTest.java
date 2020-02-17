aaf5565305130e8bc448d4958dab3101ff90e686
carloshenche <carloshenche@metricool.com>
2020-02-17 09:49:37 +0100

    Setup state machine transition

---
+++ src/test/java/guru/springframework/msscssm/config/StateMachineConfigTest.java
@@ -0,0 +1,32 @@
+package guru.springframework.msscssm.config;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import org.apache.tomcat.util.net.jsse.PEMFile;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StateMachineConfigTest {
+
+    @Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    public void testNewStateMachine(){
+        StateMachine<PaymentState, PaymentEvent> sm = this.factory.getStateMachine(UUID.randomUUID());
+        sm.start();
+        System.out.println(sm.getState().toString());
+        sm.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+        System.out.println(sm.getState().toString());
+        sm.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+        System.out.println(sm.getState().toString());
+    }
+}
